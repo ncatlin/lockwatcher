0577fe0bfc101183d0119f1660f51105aca918db
ncatlin <nia.catlin.2012@live.rhul.ac.uk>
2013-10-06 07:29:01 +0100

    now setup.py

--- src/windows/freezeit.py
+++
@@ -1,8 +0,0 @@
-import sys
-from cx_Freeze import setup, Executable
-
-setup(
-    name = "Lockwatcher",
-    version = "0.1",
-    description = "An anti-tampering monitor",
-    executables = [Executable("lockwatcher-gui.py", base = "Win32GUI")])
